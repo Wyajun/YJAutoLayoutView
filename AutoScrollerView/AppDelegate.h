84f9682bc6c7a0457ca1ebb4a713471f54db8e15
wangyajun <yajunst@163.com>
2016-04-06 09:54:03 +0800

    Initial Commit

---
+++ AutoScrollerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoScrollerView
+//
+//  Created by 王亚军 on 16/4/6.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AutoScrollerView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  AutoScrollerView
+//
+//  Created by 王亚军 on 16/4/6.
+//  Copyright © 2016年 王亚军. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
